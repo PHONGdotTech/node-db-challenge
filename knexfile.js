7e79e8e4e66cd56f00147a270eef156149c086d2
Phong Yang <phongyang88@hotmail.com>
2020-02-26 23:40:45 -0600

    removed heroku postbuild script in package.json. Refactored production connection settings in knexfile.

--- knexfile.js
+++ knexfile.js
@@ -34,13 +34,14 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      host: process.env.PG_HOST,
-      database: process.env.PG_DATABASE,
-      user: process.env.PG_USER,
-      port: process.env.PG_PORT,
-      password: process.env.PG_PASSWORD
-    },
+    connection: process.env.DATABASE_URL,
+    // connection: {
+    //   host: process.env.PG_HOST,
+    //   database: process.env.PG_DATABASE,
+    //   user: process.env.PG_USER,
+    //   port: process.env.PG_PORT,
+    //   password: process.env.PG_PASSWORD
+    // },
     pool: {
       min: 2,
       max: 10
